e97a37b302d9f84a65f18c039b1c210ed4c3c50d
Raja <ajarubab@gmail.com>
2025-05-19 17:35:50 +0530

    executing the function inside model file from controller to show it on viewpage

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     function getStudentDetails(){
+
+        $data = new \App\Models\Student;
+        echo  $data->testFun();
+
         $stdData = \App\Models\Student::all();
         return view('student', ['StdList' => $stdData]);
     }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $table = 'it_students';
+
+    function testFun(){
+        echo "This is model test function.";
+    }
 }
